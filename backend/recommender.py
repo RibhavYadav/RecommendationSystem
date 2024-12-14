35896be5c0e9a14abba008f5f009e8d43ffd24e1
draby2466 <ribhavyadav2466@gmail.com>
2024-12-14 11:34:31 +0530

    Added methods and endpoint to get the colour of the button before it is changed.

--- backend/recommender.py
+++ backend/recommender.py
@@ -2,6 +2,6 @@ import random
 import string
 
 
-def hexValueGenerator() -> str:
+def random_rgb_generator() -> str:
     color = "#" + "".join(random.choices(string.hexdigits, k=6))
     return color.upper()
--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from backend.recommender import hexValueGenerator
+from pydantic import BaseModel
+from backend.recommender import random_rgb_generator
+import uvicorn
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
+
+class ColorData(BaseModel):
+    colour: str
+
 
 @app.get("/", response_class=HTMLResponse)
 async def get_index():
     with open("frontend/index.html") as f:
         return HTMLResponse(content=f.read())
 
+
 @app.get("/random-colour")
-async def get_random_colour():
-    colour = hexValueGenerator()
+async def send_random_colour():
+    colour = random_rgb_generator()
     return {"colour": colour}
+
+
+@app.post("/get-colour")
+async def get_colour(data: ColorData):
+    return {"received_color": data.colour}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
