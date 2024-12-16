bbc7b23731608cbcd1809e96ef99cd601717d399
draby2466 <ribhavyadav2466@gmail.com>
2024-12-16 18:14:20 +0530

    Clicking a button now saves the colour of the button clicked and updates the colour of all the buttons.

--- frontend/color_updates.js
+++ frontend/color_updates.js
@@ -2,19 +2,23 @@ const colorButtons = document.querySelectorAll(".color-btn");
 
 colorButtons.forEach(button => {
     button.addEventListener('click', () => {
-        const oldColor = button.style.backgroundColor;
-        fetch("http://127.0.0.1:8000/random-colour")
-            .then(response => response.json())
-            .then(data => {
-                button.style.backgroundColor = data.colour;
-                return fetch("http://127.0.0.1:8000/get-colour", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({colour: oldColor})
-                });
-            })
-            .then(response => response.json());
+        const clickedButtonColor = button.style.backgroundColor;
+        fetch("http://127.0.0.1:8000/get-colour", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({colour: clickedButtonColor})
+        })
+        .then(response => response.json())
+        .then(() => {
+            colorButtons.forEach(btn => {
+                fetch("http://127.0.0.1:8000/random-colour")
+                    .then(response => response.json())
+                    .then(data => {
+                        btn.style.backgroundColor = data.colour;
+                    });
+            });
+        });
     });
 });
