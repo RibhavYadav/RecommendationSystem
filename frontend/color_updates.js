57f0de24e57910cdb08507a825afbe9a734ba0cc
draby2466 <ribhavyadav2466@gmail.com>
2024-12-16 18:17:44 +0530

    Clicking a button now saves the colour of the button clicked and updates the colour of all the buttons.

--- frontend/color_updates.js
+++ frontend/color_updates.js
@@ -2,13 +2,13 @@ const colorButtons = document.querySelectorAll(".color-btn");
 
 colorButtons.forEach(button => {
     button.addEventListener('click', () => {
-        const clickedButtonColor = button.style.backgroundColor;
+        const curr_colour = button.style.backgroundColor;
         fetch("http://127.0.0.1:8000/get-colour", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({colour: clickedButtonColor})
+            body: JSON.stringify({colour: curr_colour})
         })
         .then(response => response.json())
         .then(() => {
